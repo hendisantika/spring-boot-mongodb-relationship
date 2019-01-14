c4d3313fa5508818fbf9c3b63bf790dcdc8ba283
Hendi Santika <hendisantika@gmail.com>
2019-01-15 03:52:36 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmongodbrelationship/SpringBootMongodbRelationshipApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootmongodbrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongodbRelationshipApplication.class, args);
+    }
+
+}
+
